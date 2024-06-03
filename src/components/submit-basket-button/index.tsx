08a5604f95b00049b990df3b56b4118472bc16d2
alex13slem <alexshherba@gmail.com>
2024-06-04 02:20:12 +0300

    asd

--- src/components/submit-basket-button/index.tsx
+++ src/components/submit-basket-button/index.tsx
@@ -3,22 +3,17 @@ import css from './style.module.css';
 import Button from '../ui/button';
 import cn from 'classnames';
 import { useBasketStore } from '../../store/basket';
-import { useFetcher } from 'react-router-dom';
 import axios from 'axios';
 
 interface Props extends ComponentProps<FC>, HTMLAttributes<HTMLButtonElement> {}
 
 const SubmitBasketButton: FC<Props> = ({ className, ...props }) => {
-  const fetcher = useFetcher();
-
   const products = useBasketStore((state) => state.products);
 
   function handleSubmit() {
     axios.post('/api/submit-basket', products);
   }
   return (
-    // <fetcher.Form action="/api/submit-basket" method="post">
-    // <input type="hidden" name="products" value={JSON.stringify(products)} />
     <Button
       className={cn(css.root, className)}
       onClick={handleSubmit}
@@ -26,7 +21,6 @@ const SubmitBasketButton: FC<Props> = ({ className, ...props }) => {
     >
       Оформить заказ
     </Button>
-    // </fetcher.Form>
   );
 };
 
